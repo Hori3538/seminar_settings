7b3b10c8614a4edc4556648699a26c26f92f8dd1
Hori3538 <60539931+Hori3538@users.noreply.github.com>
2023-02-09 15:57:42 +0900

    Update clean_seminar_settings.sh
    changed some option

--- clean_seminar_settings.sh
+++ clean_seminar_settings.sh
@@ -6,37 +6,37 @@ BACKUP_DIR=$SCRIPT_DIR/settings_backup
 if [ -f $BACKUP_DIR/.bash_aliases ]; then
     cp $BACKUP_DIR/.bash_aliases ~/
 elif [ -f ~/.bash_aliases ]; then
-    rm -i ~/.bash_aliases
+    rm ~/.bash_aliases
 fi
 
 if [ -f $BACKUP_DIR/.bashrc ]; then
     cp $BACKUP_DIR/.bashrc ~/
 elif [ -f ~/.bashrc ]; then
-    rm -i ~/.bashrc
+    rm ~/.bashrc
 fi
 
 if [ -f $BACKUP_DIR/.cshrc ]; then
     cp $BACKUP_DIR/.cshrc ~/
 elif [ -f ~/.cshrc ]; then
-    rm -i ~/.cshrc
+    rm ~/.cshrc
 fi
 
 if [ -f $BACKUP_DIR/.inputrc ]; then
     cp $BACKUP_DIR/.inputrc ~/
 elif [ -f ~/.inputrc ]; then
-    rm -i ~/.inputrc
+    rm ~/.inputrc
 fi
 
 if [ -f $BACKUP_DIR/.logout ]; then
     cp $BACKUP_DIR/.logout ~/
 elif [ -f ~/.logout]; then
-    rm -i ~/.logout
+    rm ~/.logout
 fi
 
 if [ -f $BACKUP_DIR/.vimrc ]; then
     cp $BACKUP_DIR/.vimrc ~/
 elif [ -f ~/.vimrc ]; then
-    rm -i ~/.vimrc
+    rm ~/.vimrc
 fi
 
 if [ -d $BACKUP_DIR/.vim ]; then
