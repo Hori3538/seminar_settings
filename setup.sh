2932c0253c264e7b8f4d10bc2d3621314ab9ada4
Hori3538 <60539931+Hori3538@users.noreply.github.com>
2023-03-01 10:39:04 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,26 +11,6 @@ cat $SCRIPT_DIR/bashrc >> ~/.bashrc
 #vim settings
 echo "source $SCRIPT_DIR/vimrc" >> ~/.vimrc
 
-
-echo "setting vim"
-git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
-vim_version=`vim --version | grep -o '[0-9]*'`
-vim_version=${vim_version:0:1}
-
-if [ $((${vim_version})) -lt 8 ]; then
-    echo "installed vim version < 8"
-    cd ~/.vim/dein/repos/github.com/Shougo/dein.vim
-    git checkout 1.5
-    cd $SCRIPT_DIR
-else
-    echo "installed vim version >= 8"
-fi
-
-mkdir ~/.vim/undo
-
-echo "open vim and run ':call dein#install()'"
-echo "setting vim end"
-
 echo "installing powerline font"
 git clone https://github.com/powerline/fonts.git --depth=1 /tmp/powerline_fonts \
 && cd /tmp/powerline_fonts \
